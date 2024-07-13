2db183d73dbc5ee643a17594ffcd93c214a5bce0
David Adisa <adisad377@gmail.com>
2024-07-13 20:57:46 +0100

    in control

--- src/components/productsection/ProductSection.jsx
+++ src/components/productsection/ProductSection.jsx
@@ -24,64 +24,6 @@ const ProductSection = ({header, show, setShow, getItemForDescription}) => {
     });
   },[])
 
-  // const items = [
-  //   {
-  //     id: uuidv4(),
-  //     src :"https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
-  //     name: "Chocolate cake",
-  //     price: 10,
-  //     sizes : ["S","M","L"]
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     src :"https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
-  //     name: "Chocolate cake",
-  //     price: 10,
-  //     sizes : ["S","M","L"]
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     src :"https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
-  //     name: "Chocolate cake",
-  //     price: 10,
-  //     sizes : ["S","M","L"]
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     src :"https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
-  //     name: "Chocolate cake",
-  //     price: 10,
-  //     sizes : ["S","M","L"]
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     src :"https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
-  //     name: "Chocolate cake",
-  //     price: 10,
-  //     sizes : ["S","M","L"]
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     src :"https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
-  //     name: "Chocolate cake",
-  //     price: 10,
-  //     sizes : ["S","M","L"]
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     src :"https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
-  //     name: "Chocolate cake",
-  //     price: 10,
-  //     sizes : ["S","M","L"]
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     src :"https://images.unsplash.com/photo-1606890737304-57a1ca8a5b62?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
-  //     name: "Chocolate cake",
-  //     price: 10,
-  //     sizes : ["S","M","L"]
-  //   }
-  // ]
   return (
     <>
       <div className="mt-20 pb-10">
@@ -105,8 +47,8 @@ const ProductSection = ({header, show, setShow, getItemForDescription}) => {
             <div className="px-8 md:px-[12vw] flex-col md:flex-row flex flex-wrap w-full gap-16 md:gap-2">
               {
                 items?.length>0 && 
-                items.filter((item)=>item?.categories[0].name===header.toLowerCase())
-                      .slice(0,6)
+                items?.filter((item)=>item?.categories[0].name===header.toLowerCase())
+                      ?.slice(0,6)
                       .map((item, index)=>(
                         <CakeCard show={show} setShow={setShow} key={index} item={item} getItemForDescription={getItemForDescription}/>
                       ))
